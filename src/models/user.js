28909fa1032e9cb575146a4db348a8888eec0094
Giovanna-Cynthia <giovannacynthia34@gmail.com>
2024-08-08 14:14:01 -0300

    Inserindo banco de dado

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/config");
+
+const User = sequelize.define('user', {
+    nome: {
+        type: DataTypes.STRING, //Somente letras
+        allowNull: false //Nunca pode ser nulo
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./router/router');
+const sequelize = require('./config/config');
 const app = express();
 
 //modelo da API JSON
@@ -17,9 +18,36 @@ app.get('/healthcheck', (req, res) => {
 });
 
 //Listen -> ouvir (8080)
-app.listen(8080, () => {
+/*app.listen(8080, () => {
+
+    sequelize
+    
     console.log("##############");
     console.log("Estamos online na http: 8080");
     console.log("##############");
 
+});*/
+sequelize.authenticate()
+// Irá verificar se tem o banco de dados criado
+.then(() =>{
+    console.log("Conexão estabelecida com sucesso");
+    return sequelize.sync();
+})
+//Caso tenha o banco de dados criado, irá rodar, se não dará erro
+.then(() => {
+    app.listen(8080, () => {
+    console.log("##############");
+    console.log("Rodando na porta 8080");
+    console.log("##############");
+    });
+})
+.catch((error) => {
+    console.log("Erro ao se conectar com o banco: ", error);
 });
+
+/*
+//é como se fosse um try/catch = porem mais antigo
+.then
+.catch
+try{
+}catch*/
